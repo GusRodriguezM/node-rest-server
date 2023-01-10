6782c6fae67d4882dc5402d8c10ef27f9943c49a
Gustavo RM <gustavo.rm.isc@gmail.com>
2023-01-10 00:20:35 -0600

    Adding a User model schema

---
+++ models/user.js
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+
+const UserScheme = Schema({
+    name: {
+        type: String,
+        required: [true, 'The name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'The email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'The password is required']
+    },
+    image: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean, 
+        default: false
+    }
+});
+
+export default model( 'User', UserScheme );
