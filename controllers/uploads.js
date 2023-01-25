1146eb940bddc40c2b35dec452526f508f08cfae
Gustavo RM <gustavo.rm.isc@gmail.com>
2023-01-25 01:15:02 -0600

    Creating a helper to upload a file in a specified folder if required

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,11 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { v4 as uuidv4 } from 'uuid';
 import { request, response } from "express";
+import { uploadFile } from "../helpers/index.js";
 
-const __dirname = path.dirname( fileURLToPath( import.meta.url ) );
-
-export const uploadFiles = ( req = request, res = response) => {
+//Controller to upload files to the server
+export const uploadFiles = async( req = request, res = response) => {
 
     if ( !req.files || Object.keys(req.files).length === 0 || !req.files.file ) {
         res.status(400).json({
@@ -13,34 +10,22 @@ export const uploadFiles = ( req = request, res = response) => {
         });
         return;
     }
-    
-    const { file } = req.files;
 
-    //Splitting the file name into elemetens of an array
-    const shortName = file.name.split('.');
-    
-    //Getting the extension of the file
-    const extension = shortName[ shortName.length - 1 ];
+    /**
+     * Function to upload files. Receives three args
+     * 1. The files from the request
+     * 2. The specified extensions, if empty or undefined the function will accept only images
+     * 3. The folder name to upload the files
+     */
 
-    //Comparing the extension against an allowed list of extensions
-    const validExtensions = [ 'png', 'jpg', 'jpeg', 'gif' ];
-
-    if( !validExtensions.includes( extension ) ){
-        return res.status(400).json({
-            msg: `The extension ${extension} is not allowed. The allowed extensions are ${validExtensions}`
-        });
+    try {
+        //Example using different arguments
+        // const fileName = await uploadFile( req.files, ['txt', 'md'], 'texts' );
+        //Accepting only images
+        const fileName = await uploadFile( req.files, undefined, 'imgs' );
+        res.json({ fileName });
+    } catch ( error ) {
+        res.status(400).json({ msg: error });
     }
-
-    const tempName = uuidv4() + '.' + extension;
-    const uploadPath = path.join( __dirname, '../uploads/', tempName );
-
-    file.mv( uploadPath, (err) => {
     
-        if ( err ) {
-            return res.status(500).json({ err });
-        }
-
-        res.json( { msg: 'File uploaded to ' + uploadPath } );
-
-    });
 }
