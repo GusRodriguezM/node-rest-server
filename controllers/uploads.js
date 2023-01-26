b983fcd3fa54318e44c7584f7897791701ade132
Gustavo RM <gustavo.rm.isc@gmail.com>
2023-01-26 00:30:07 -0600

    Creating a middleware to check if the request contains a file to upload

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -5,13 +5,6 @@ import { Product, User } from "../models/index.js";
 //Controller to upload files to the server
 export const uploadFiles = async( req = request, res = response) => {
 
-    if ( !req.files || Object.keys(req.files).length === 0 || !req.files.file ) {
-        res.status(400).json({
-            msg: 'There is no files in the request'
-        });
-        return;
-    }
-
     /**
      * Function to upload files. Receives three args
      * 1. The files from the request
--- routes/uploads.js
+++ routes/uploads.js
@@ -2,15 +2,16 @@ import { Router } from "express";
 import { body, param } from 'express-validator';
 import { updateImage, uploadFiles } from "../controllers/uploads.js";
 import { allowedCollections } from "../helpers/index.js";
-import { validateFields } from "../middlewares/index.js";
+import { validateFields, validateFileToUpload } from "../middlewares/index.js";
 
 export const uploadFilesRouter = Router();
 
 //GET API Route to upload a file
-uploadFilesRouter.post( '/', uploadFiles );
+uploadFilesRouter.post( '/', validateFileToUpload, uploadFiles );
 
 //PUT API Route to change the image of the user
 uploadFilesRouter.put('/:collection/:id', [
+    validateFileToUpload,
     param( 'id', 'The id is not valid' ).isMongoId(),
     //Using a callback to receive the collection and the list of allowed collections
     param( 'collection' ).custom( c => allowedCollections( c, ['users', 'products'] ) ),
